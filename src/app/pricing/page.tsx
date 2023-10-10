09f1c62c8d80857e5cae962644c93e60c231ff74
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2023-10-10 07:46:48 +0000

    complete pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,5 +1,5 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import NavBar from "@/components/NavBar";
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import NavBar from '@/components/NavBar';
 import {
   Card,
   CardContent,
@@ -7,97 +7,178 @@ import {
   CardHeader,
   CardTitle,
   CardFooter,
-} from "@/components/ui/card";
-import { cn } from "@/lib/utils";
+} from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import { BsCheck2 } from 'react-icons/bs';
+import { AiOutlineMinus } from 'react-icons/ai';
+import { Separator } from '@/components/ui/separator';
+import { AiOutlineQuestionCircle } from 'react-icons/ai';
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card';
+import { Button } from '@/components/ui/button';
 
 const PricingPage = () => {
   return (
-    <MaxWidthWrapper>
-      <div className="border-b">
-        <MaxWidthWrapper>
-          <NavBar />
-        </MaxWidthWrapper>
-      </div>
-      <div className="flex gap-8">
-        <Card className={cn("w-72")}>
-          <CardHeader className="text-center">
-            <CardTitle>Free</CardTitle>
-            <CardDescription>For Hobby Projects</CardDescription>
-          </CardHeader>
-          <CardHeader className="text-center pt-0">
-            <CardTitle className="text-4xl">$0</CardTitle>
-            <CardDescription>per month</CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-2 text-gray-600 text-sm">
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>10 PDFs included</p>
+    <div className="min-h-screen">
+      <MaxWidthWrapper>
+        <NavBar />
+      </MaxWidthWrapper>
+      <Separator />
+      <MaxWidthWrapper className="mt-8">
+        <div className="flex justify-center gap-8">
+          <Card className={cn('w-72')}>
+            <CardHeader className="text-center">
+              <CardTitle>Free</CardTitle>
+              <CardDescription>For Hobby Projects</CardDescription>
+            </CardHeader>
+            <CardHeader className="text-center pt-0">
+              <CardTitle className="text-4xl">$0</CardTitle>
+              <CardDescription>per month</CardDescription>
+            </CardHeader>
+
+            <Separator />
+
+            <CardContent className="flex flex-col gap-4 text-gray-600 text-sm mt-8">
+              <div className="flex items-center gap-4">
+                <BsCheck2 size={20} className="text-gray-500" />
+                <p>10 PDFs included</p>
+              </div>
+              <div className="flex items-center gap-4">
+                <BsCheck2 size={20} className="text-gray-500" />
+                <div className="flex items-center gap-2">
+                  <p>6 pages per PDF</p>
+                  <HoverCard>
+                    <HoverCardTrigger>
+                      <AiOutlineQuestionCircle />
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-[24]">
+                      A single PDF can have maximum 6 pages.
+                    </HoverCardContent>
+                  </HoverCard>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <BsCheck2 size={20} className="text-gray-500" />
+                <div className="flex items-center gap-2">
+                  <p>5MB file size limit</p>
+                  <HoverCard>
+                    <HoverCardTrigger>
+                      <AiOutlineQuestionCircle />
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-[24]">
+                      A single PDF size cannot exceed 5MB.
+                    </HoverCardContent>
+                  </HoverCard>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <BsCheck2 size={20} className="text-gray-500" />
+                <p>Mobile friendly interface</p>
+              </div>
+              <div className="flex items-center gap-4 text-gray-400">
+                <AiOutlineMinus size={20} />
+                <div className="flex items-center gap-2">
+                  <p>Higher quality responses</p>
+                  <HoverCard>
+                    <HoverCardTrigger>
+                      <AiOutlineQuestionCircle />
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-[24]">
+                      Less powerful Machine learning model will be used in Free
+                      plan
+                    </HoverCardContent>
+                  </HoverCard>
+                </div>
+              </div>
+              <div className="flex items-center gap-4 text-gray-400">
+                <AiOutlineMinus size={20} />
+                <p>Priority support</p>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">Signup</Button>
+            </CardFooter>
+          </Card>
+          <Card className={cn('w-72 border-[#E53B5B] border-2 relative')}>
+            <div className="bg-[#E53B5B] absolute right-[32%] -top-4 text-white p-2 rounded-lg text-xs">
+              Most Popular
             </div>
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>6 pages per PDF</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>5MB file size limit</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>Mobile friendly interface</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>Higher quality responses</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>Priority support</p>
-            </div>
-          </CardContent>
-        </Card>
-        <Card className={cn("w-72")}>
-          <CardHeader className="text-center">
-            <CardTitle>Pro</CardTitle>
-            <CardDescription>
-              For large projects with higher needs
-            </CardDescription>
-          </CardHeader>
-          <CardHeader className="text-center pt-0">
-            <CardTitle className="text-4xl">$8</CardTitle>
-            <CardDescription>per month</CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-2 text-gray-600 text-sm">
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>30 PDFs included</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>6 pages per PDF</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>5MB file size limit</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>Mobile friendly interface</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>Higher quality responses</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="flex h-2 w-2 rounded-full bg-sky-500" />
-              <p>Priority support</p>
-            </div>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footerr</p>
-          </CardFooter>
-        </Card>
-      </div>
-    </MaxWidthWrapper>
+            <CardHeader className="text-center">
+              <CardTitle>Pro</CardTitle>
+              <CardDescription>For Large Projects</CardDescription>
+            </CardHeader>
+            <CardHeader className="text-center pt-0">
+              <CardTitle className="text-4xl">$8</CardTitle>
+              <CardDescription>per month</CardDescription>
+            </CardHeader>
+            <Separator />
+            <CardContent className="flex flex-col gap-4 text-gray-600 text-sm mt-8">
+              <div className="flex items-center gap-4">
+                <BsCheck2 size={20} className="text-gray-500" />
+                <p>30 PDFs included</p>
+              </div>
+              <div className="flex items-center gap-4">
+                <BsCheck2 size={20} className="text-gray-500" />
+                <div className="flex items-center gap-2">
+                  <p>25 pages per PDF</p>
+                  <HoverCard>
+                    <HoverCardTrigger>
+                      <AiOutlineQuestionCircle />
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-[24]">
+                      A single PDF can have maximum 25 pages.
+                    </HoverCardContent>
+                  </HoverCard>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <BsCheck2 size={20} className="text-gray-500" />
+                <div className="flex items-center gap-2">
+                  <p>15MB file size limit</p>
+                  <HoverCard>
+                    <HoverCardTrigger>
+                      <AiOutlineQuestionCircle />
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-[24]">
+                      A single PDF size cannot exceed 15MB.
+                    </HoverCardContent>
+                  </HoverCard>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <BsCheck2 size={20} className="text-gray-500" />
+                <p>Mobile friendly interface</p>
+              </div>
+              <div className="flex items-center gap-4">
+                <BsCheck2 size={20} className="text-gray-500" />
+                <div className="flex items-center gap-2">
+                  <p>Higher quality responses</p>
+                  <HoverCard>
+                    <HoverCardTrigger>
+                      <AiOutlineQuestionCircle />
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-[24]">
+                      More powerful Machine learning model will be used in Pro
+                      plan
+                    </HoverCardContent>
+                  </HoverCard>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <BsCheck2 size={20} className="text-gray-500" />
+                <p>Priority support</p>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">Signup</Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </MaxWidthWrapper>
+    </div>
   );
 };
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,29 @@
-import Link from "next/link";
-import { Button } from "./ui/button";
+'use client';
 
-
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineClose } from 'react-icons/ai';
+import { useState } from 'react';
+import { RegisterLink, LoginLink } from '@kinde-oss/kinde-auth-nextjs/server';
 
 const NavBar = () => {
+  const [openMobileNav, setOpenMobileNav] = useState(false);
+
+  const openMobileNavbar = () => {
+    setOpenMobileNav(true);
+  };
+
+  const closeMobileNavbar = () => {
+    setOpenMobileNav(false);
+  };
+
   return (
-    <nav className="flex items-center justify-between py-4 ">
-      <p className="text-lg font-semibold">PDF-Chat</p>
-      <ul className="flex items-center gap-8 text-sm">
+    <nav className="flex items-center justify-between py-4 relative">
+      <Link href="/" className="text-sm md:text-base lg:text-lg font-semibold">
+        PDF-Chat
+      </Link>
+      <ul className="hidden md:flex items-center gap-8 text-sm">
         <li className="text-gray-600">
           <Link href="/pricing">Pricing</Link>
         </li>
@@ -15,9 +31,57 @@ const NavBar = () => {
           <Link href="/signin">Signin</Link>
         </li>
         <li>
-          <Link href="/signup"><Button size="sm">Get Started</Button></Link>
+          <Link href="/signup">
+            <Button size="sm">Get Started</Button>
+          </Link>
         </li>
+        <LoginLink>Sign in</LoginLink>
+
+        <RegisterLink>Sign up</RegisterLink>
       </ul>
+
+      {/* Small Screen NavBar */}
+      {!openMobileNav && (
+        <div
+          className="md:hidden bg-gray-100 px-4 py-2 rounded-lg cursor-pointer"
+          onClick={openMobileNavbar}
+        >
+          <RxHamburgerMenu />
+        </div>
+      )}
+
+      {openMobileNav && (
+        <div className="h-screen w-64 absolute right-0 top-2 border bg-white rounded-lg z-[100]">
+          <div className="relative">
+            <AiOutlineClose
+              className="absolute right-4 top-4 cursor-pointer"
+              onClick={closeMobileNavbar}
+            />
+          </div>
+
+          <ul className="flex px-4 py-12 gap-4 text-sm flex-col">
+            <li className="text-gray-600 ">
+              <Link href="/pricing">
+                <Button variant="link" size="sm" className="px-0">
+                  Pricing
+                </Button>
+              </Link>
+            </li>
+            <li className="text-gray-600">
+              <Link href="/signin">
+                <Button variant="link" size="sm" className="px-0">
+                  Signin
+                </Button>
+              </Link>
+            </li>
+            <li>
+              <Link href="/signup">
+                <Button size="sm">Get Started</Button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
